37927319cbbc1a5446f704076b42e3b3b6006593
GGORG <GGORG0@protonmail.com>
2025-04-08 21:52:32 +0200

    added blur to navbar

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -26,7 +26,7 @@ export default function SiteHeader() {
   const items = isAdmin ? navItems.admin : navItems.public;
 
   return (
-    <header className="sticky top-0 z-40 w-full border-b">
+    <header className="sticky top-0 z-40 w-full border-b backdrop-blur-sm">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={items} admin={isAdmin} />
         <div className="flex flex-1 items-center justify-end space-x-4">
