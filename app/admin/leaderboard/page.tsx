fe2615a766509ec6b4149e9fb0905310ba7176ab
GGORG <GGORG0@protonmail.com>
2025-04-10 19:23:18 +0200

    made the leaderboard actually function (temp fix)

--- app/admin/leaderboard/page.tsx
+++ app/admin/leaderboard/page.tsx
@@ -4,44 +4,61 @@ import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
 
 import { Extensible } from '@/lib/models';
-import LeaderboardUser from '@/lib/models/leaderboard_user';
+import LeaderboardUser, {
+  leaderboard_columns,
+} from '@/lib/models/leaderboard_user';
 import { useLoggedInSurrealClient } from '@/hooks/surreal-provider';
-import { Spinner } from '@/components/ui/spinner';
-import AdminLeaderboardTable from '@/components/admin/leaderboard-table';
+import { DataTable } from '@/components/ui/data-table';
 
 export default function AdminLeaderboard() {
   const surreal = useLoggedInSurrealClient();
   const [data, setData] = useState<LeaderboardUser[]>([]);
-  const [isFetching, setIsFetching] = useState(true);
 
   useEffect(() => {
     if (!surreal) return;
 
+    let timeout: NodeJS.Timeout | null = null;
+
     const fetchTasks = async () => {
       try {
-        const fetchedData =
-          await surreal.select<Extensible<LeaderboardUser>>('leaderboard_user');
+        const fetchedData = (
+          await surreal.query<Extensible<LeaderboardUser>[][]>(`
+          SELECT
+            name,
+            math::sum(->discovered->task.points_discovered) + math::sum(->(submitted WHERE correct = true)->task.points_solved) AS points,
+            count(->discovered) AS discover_count,
+            count(->submitted) as answer_count,
+            count(->(submitted WHERE correct = true)) AS solve_count
+          FROM user
+          WHERE count(->discovered) > 0
+          ORDER BY points DESC
+          LIMIT 100;
+        `)
+        )[0];
 
         setData(fetchedData);
-
-        console.log('Fetched initial data');
-        setIsFetching(false);
       } catch (err) {
         console.error('Failed to fetch tasks:', err);
         toast.error('Nie udało się pobrać zadań');
       }
+
+      timeout = setTimeout(() => {
+        fetchTasks();
+      }, 10000);
     };
 
     fetchTasks();
+
+    return () => {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+    };
   }, [surreal]);
 
-  if (isFetching) {
-    return (
-      <div className="flex h-full items-center justify-center">
-        <Spinner />
-      </div>
-    );
-  }
-
-  return <AdminLeaderboardTable initialData={data} />;
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={leaderboard_columns} data={data} />
+    </div>
+  );
 }
