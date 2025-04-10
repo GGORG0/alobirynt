fd7746ee4ef45b9fff26df19ff40a30abf998fef
GGORG <GGORG0@protonmail.com>
2025-04-10 21:11:33 +0200

    added database purge button

--- app/admin/leaderboard/page.tsx
+++ app/admin/leaderboard/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { Trash2 } from 'lucide-react';
 import { toast } from 'sonner';
 
 import { Extensible } from '@/lib/models';
@@ -8,6 +9,18 @@ import LeaderboardUser, {
   leaderboard_columns,
 } from '@/lib/models/leaderboard_user';
 import { useLoggedInSurrealClient } from '@/hooks/surreal-provider';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
 import { DataTable } from '@/components/ui/data-table';
 import { Label } from '@/components/ui/label';
 
@@ -19,11 +32,17 @@ export default function AdminLeaderboard() {
   const [refreshInterval, setRefreshInterval] = useState<number>(10000);
 
   useEffect(() => {
-    if (!surreal) return;
-
     let timeout: NodeJS.Timeout | null = null;
 
     const fetchTasks = async () => {
+      if (!surreal) {
+        console.error('Surreal client is not initialized');
+        toast.error(
+          'Nie udało się pobrać zadań: brak połączenia z bazą danych'
+        );
+        return;
+      }
+
       try {
         const fetchedData = (
           await surreal.query<Extensible<LeaderboardUser>[][]>(`
@@ -60,6 +79,28 @@ export default function AdminLeaderboard() {
     };
   }, [refreshInterval, surreal]);
 
+  const purgeDatabase = useCallback(async () => {
+    if (!surreal) {
+      console.error('Surreal client is not initialized');
+      toast.error(
+        'Nie udało się wyczyścić bazy danych: brak połączenia z bazą danych'
+      );
+      return;
+    }
+
+    try {
+      await surreal.delete('submitted');
+      await surreal.delete('discovered');
+      await surreal.delete('user');
+
+      console.log('Purged DB');
+      toast.success('Wyczyszczono bazę danych');
+    } catch (err) {
+      console.error('Failed to purge DB:', err);
+      toast.error('Nie udało się wyczyścić bazy danych');
+    }
+  }, [surreal]);
+
   return (
     <div className="container mx-auto flex flex-col gap-4 py-10">
       <h1 className="text-3xl font-bold">Tablica wyników</h1>
@@ -82,6 +123,30 @@ export default function AdminLeaderboard() {
         />
       </div>
       <DataTable columns={leaderboard_columns} data={data} />
+
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant="destructive" className="cursor-pointer">
+            Wyczyść bazę danych <Trash2 />
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Czy jesteś pewny?</AlertDialogTitle>
+            <AlertDialogDescription>
+              To usunie wszystkie konta uczestników, ich odkrycia i przesłane
+              odpowiedzi. Zadania nie zostaną usunięte. Nie można cofnąć tej
+              akcji.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Anuluj</AlertDialogCancel>
+            <AlertDialogAction onClick={purgeDatabase}>
+              Kontynuuj
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
